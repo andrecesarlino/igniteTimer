50974fe5f2e01cd8357c5a79e7c6a6df49089498
Andre Lino <andreclino@gmail.com>
2023-01-23 12:14:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+
+export function App() {
+
+  return (
+  <>
+   <Button variant="success"/>
+   <Button variant="danger" />
+   <Button variant="secundary" />
+   <Button variant="primary" />
+  </>
+  )
+  
+}
+
